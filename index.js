5630b22933df002a7cd4c190aa5a5af1661fe6e6
anshanto <asikurjamann@gmail.com>
2022-10-21 04:01:13 +0600

    index add

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(cors());
 
 
 const categories = require('./Data/categories.json');
+const news = require('./Data/news.json');
 
 app.get('/', (req, res) => {
     res.send('hellow world')
@@ -15,6 +16,27 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id)
+        res.send(category_news);
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews);
 })
 
 app.listen(port, () => {
